437bd39f42ccf84a80c42e6e22356ee630fb4204
unkuseni <amudalatadeoye@gmail.com>
2023-09-24 00:23:43 +0100

    learning about concurrency

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::{sync::mpsc, sync::Mutex, thread, time::Duration};
+
+fn main() {
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(0));
+        }
+    });
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(5));
+        }
+    });
+    for received in rx {
+        println!("Got: {}", received);
+    }
+    let m = Mutex::new(5);
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    println!("m = {:?}", m);
+}
